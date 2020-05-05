015ffc504040909a0327e44e94c350ed697d351f
Alexandre Reed <ch.alexandre.reed@gmail.com>
2020-05-06 00:11:39 +0200

    enable dark mode

--- lib/controllers/colorController.dart
+++ lib/controllers/colorController.dart
@@ -3,29 +3,28 @@ import 'dart:ui';
 import 'package:onregardequoicesoir/constants/constants.dart' as Constants;
 
 class ColorController {
-
   Color background;
-
   Color text;
+  bool dark;
 
   ColorController() {
     this.background = Constants.dark;
 
     this.text = Constants.white;
+
+    this.dark = true;
   }
 
-  void darkMode() {
-    background = Constants.dark;
-
-    text = Constants.white;
+  void changeColorMode() {
+    dark = !dark;
+    if (dark) {
+      this.background = Constants.dark;
+      this.text = Constants.white;
+    } else {
+      this.background = Constants.white;
+      this.text = Constants.dark;
+    }
   }
-
-  void lightMode() {
-    background = Constants.white;
-
-    text = Constants.dark;
-  }
-
 }
 
 final ColorController colorController = new ColorController();
--- lib/views/loginScreen.dart
+++ lib/views/loginScreen.dart
@@ -1,11 +1,18 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:onregardequoicesoir/controllers/colorController.dart';
 
 class LoginScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
-    return Text("caca", style: TextStyle(color: Colors.white),);
+    return Scaffold(
+        backgroundColor: colorController.background,
+        body: Center(
+          child: Text(
+            "caca",
+            style: TextStyle(color: colorController.text),
+          ),
+        ));
   }
-
 }
